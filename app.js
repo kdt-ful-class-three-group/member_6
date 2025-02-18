ebbe4c06e3915abbe73f214a928af31e79841830
ahn-eunbyul-1103 <greenahneb@gmail.com>
2025-02-18 18:02:58 +0900

    Feat,Fix: 변수 선언으로 데이터 조회해오기

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import fs from 'fs';
 import qs from 'querystring'; // 쿼리스트링 라이브러리 추가
+import loginData from './loginData.js';
 
 const server = http.createServer(function(req, res) {
   if(req.method === "GET") {
@@ -13,13 +14,69 @@ const server = http.createServer(function(req, res) {
   }
   if(req.method === "POST") {
     if(req.url === "/form") {
+      let body = "";
+      let dataObj;
       req.on("data", function(data) {
-        console.log(qs.parse(data.toString())); // 문자열을 JSON Obj형식으로 변환
+        body += data;
+        console.log(qs.parse(body)); // 문자열을 JSON Obj형식으로 변환
+        dataObj = qs.parse(body);
       });
-      const resultPage = fs.readFileSync('result.html', 'utf-8');
-      res.writeHead( 200, { "Content-Type": "text/html; charset=utf-8" } );
-      res.write(resultPage);
-      res.end();
+      req.on("end", function() {
+        let name = dataObj.name;
+        let password = dataObj.password;
+        let loginFlg = false;
+        loginData.forEach(function(users) {
+          if(users.name == name && users.password == password) {
+            loginFlg = true;
+            return loginFlg;
+          }
+        });
+        if(loginFlg === true) {
+          // const resultPage = fs.readFileSync('result.html', 'utf-8');
+          res.writeHead( 200, { "Content-Type": "text/html; charset=utf-8" } );
+          res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>로그인 결과 화면</title>
+</head>
+<body>
+  <h1>post 반응하고 왔어!</h1>
+  <p>이름 : ${name}</p>
+  <p>비밀번호 : ${password}</p>
+  <p>로그인 성공했어!</p>
+</body>
+</html>`);
+        } else {
+          res.writeHead(404);
+      res.end(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인 결과 화면</title>
+  </head>
+  <body>
+    <h1>post 반응하고 왔어!</h1>
+    <p>로그인 실패했어 ㅠㅠ</p>
+  </body>
+  </html>`);
+        }
+      });
+    } else {
+      res.writeHead(404);
+      res.end(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>잘못된 접근</title>
+  </head>
+  <body>
+    <h1>잘못된 페이지야!</h1>
+  </body>
+  </html>`);
     }
   }
 });
---
+++ loginData.js
@@ -0,0 +1,6 @@
+const loginData = [
+  { name: "안은별", password: "1234" },
+  { name: "박준배", password: "0926" }
+];
+
+export default loginData;

9ba26b33b72f96a84fb2cfa0b15327baab504421
ahn-eunbyul-1103 <greenahneb@gmail.com>
2025-02-18 15:54:06 +0900

    Feat,Comment: querystring추가 -> json obj 형식 반환 확인

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import fs from 'fs';
+import qs from 'querystring'; // 쿼리스트링 라이브러리 추가
 
 const server = http.createServer(function(req, res) {
   if(req.method === "GET") {
@@ -12,6 +13,9 @@ const server = http.createServer(function(req, res) {
   }
   if(req.method === "POST") {
     if(req.url === "/form") {
+      req.on("data", function(data) {
+        console.log(qs.parse(data.toString())); // 문자열을 JSON Obj형식으로 변환
+      });
       const resultPage = fs.readFileSync('result.html', 'utf-8');
       res.writeHead( 200, { "Content-Type": "text/html; charset=utf-8" } );
       res.write(resultPage);
